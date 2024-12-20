5fd421522bc48161044d165e9b5220bd73655585
Rahul Naskar <naskarrahul620@gmail.com>
2024-12-20 17:36:39 +0530

    .

--- client/src/components/Editor/ExecuteButton.tsx
+++ client/src/components/Editor/ExecuteButton.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Play } from 'lucide-react';
 
 interface ExecuteButtonProps {
--- client/src/components/RoomInfo.tsx
+++ client/src/components/RoomInfo.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Users } from 'lucide-react';
 import { useEditorStore } from '../store/editorStore';
 
